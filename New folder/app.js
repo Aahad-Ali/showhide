899aacc3b5c14584ef52de39eda023d20ff6d6ee
Aahad Ali <104500845+Aahad-Ali@users.noreply.github.com>
2022-08-11 21:18:45 +0500

    Add files via upload

---
+++ New folder/app.js
@@ -0,0 +1,9 @@
+function expand() {
+    let paragraph = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, vel recusandae temporibus laborum quaeratLorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, vel recusandae temporibus laborum quaeratLorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, vel recusandae temporibus laborum quaerat"
+    document.getElementById("para").innerHTML = paragraph;
+    
+}
+
+function again(){
+    let hide = document.getElementById("para").innerHTML = para;
+}
